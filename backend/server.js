c11739aca0f2bc8e6bb6255a2d5bd62b02079cce
Christopher Fradella <chris@snake.local>
2020-10-03 14:36:28 -0700

    Added dotenv and concurrently. Cleaned up ES6 syntax

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const products = require("./data//products")
+import express from "express";
+import products from "./data//products.js";
+import dotenv from "dotenv";
 const app = express();
 
+dotenv.config();
 
 app.get("/", (req,res) => {
     res.send("API is running.")
@@ -16,4 +18,6 @@ app.get("/api/products/:id", (req,res) => {
     res.json(product)
 })
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => { console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}...`)})
