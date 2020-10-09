9de5594326cfc8055f1e955b3041c41573eef999
Christopher Fradella <chris@snake.local>
2020-10-08 23:15:51 -0700

    joke

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -14,19 +14,19 @@ const SearchBox = ({ history }) => {
     }
 
     return (
-        <Form onSubmit={submitHandler} style={{ width:"400px" }} inline>
+        <Form onSubmit={submitHandler} inline>
             <Form.Control 
                 type="text"
                 name="q"
                 onChange={e => setKeyword(e.target.value)}
                 placeholder="Search products..."
-                className="mr-sm-2 ml-sm-5 rounded">
+                className="mr-sm-2 ml-sm-5 rounded"
+                style={{ width:"600px" }}>
             </Form.Control>
             <Button 
                 type="submit"
                 variant="outline-success"
-                className="p-2 rounded"
-                >
+                className="p-2 rounded">
                 Search
             </Button>
         </Form>
